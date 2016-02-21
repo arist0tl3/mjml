03d4cf1715d302bdc9a9716d568efd07f83fdea1
Sean <sean@vigorwebsolutions.com>
2016-02-21 13:30:23 -0800

    Update mjml2html.js

--- src/mjml2html.js
+++ src/mjml2html.js
@@ -207,6 +207,9 @@ const internals = {
     return `<!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+  <style>
+    .sean { width:10px }
+  </style>
   ${extraFonts}
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <title>${title}</title>
